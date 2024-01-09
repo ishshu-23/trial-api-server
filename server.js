56cbb7c4fb49c033a6bdb3ac89ed9e4bd8e81b1c
ishshu <120395843+ishshu-23@users.noreply.github.com>
2024-01-09 22:39:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,45 @@
 const express = require("express");
+const channelRouter = require("./routes/channel.router");
+const usersRouter = require("./routes/users.router");
+const { iteratorLoop } = require("./controllers/match.controller.js");
+const { countries } = require('./models/countries.model.js');
+const { users } = require('./models/users.model.js');
+
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("/ url is working");
-});
-
-app.get("/users", (req, res) => {
-    const userData = {
-        message: "users endpoint is working",
-        // Include any other user-related data here
+    // Creating a JSON object to send as a response
+    const jsonResponse = {
+        message: "Server is working",
+        status: "success"
     };
 
-    // Sending a JSON response
-    res.json(userData);
+    // Sending the JSON response
+    res.json(jsonResponse);
 });
 
-app.get("/channels", (req, res) => {
-    res.send("channels endpoint is working");
-});
+
+app.use("/channel", channelRouter);
+app.use("/users", usersRouter);
+
+// iteratorLoop();
+// setInterval(() => { 
+//     console.log("Hello World");
+
+//     var index1 = Math.floor(Math.random() * countries.length);
+//     var index2 = Math.floor(Math.random() * countries.length);
+//     if (index1 === index2 || users[index1].length === 0 || users[index2].length === 0) {
+//         console.log("I am inside if condition");
+//     } else {
+//         var user1 = users[index1].shift();
+//         var userid1 = user1.userid;
+//         var user2 = users[index2].shift();
+//         var userid2 = user2.userid;
+//         console.log("User1 id", userid1, "User2 id", userid2);
+//         user1.res.send(userid1 + userid2);
+//         user2.res.send(userid1 + userid2);
+//     }
+// }, 1000);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
